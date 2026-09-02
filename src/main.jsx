260c4306ffc347b0731d6fc3a9f3c4c6ac625327
vasanth-rlogics <vasanth.gopinath@rlogicssolutions.com>
2026-09-02 14:05:07 +0530

    Load registration stepper viewport fixes

--- src/main.jsx
+++ src/main.jsx
@@ -2,5 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './styles.css';
+import './stepper-fix.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(<React.StrictMode><App /></React.StrictMode>);
