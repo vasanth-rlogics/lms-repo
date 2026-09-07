6725233e445f71c77f0e4e5a43b0d12b80879e45
vasanth-rlogics <vasanth.gopinath@rlogicssolutions.com>
2026-09-07 13:36:23 +0530

    Mount loan closure API

--- functions/customer_auth_api/bootstrap.js
+++ functions/customer_auth_api/bootstrap.js
@@ -1,4 +1,6 @@
 const app=require('./index');
 const applicationRoutes=require('./applicationRoutes');
+const closureRoutes=require('./closureRoutes');
 app.use('/api/applications',applicationRoutes);
+app.use('/api/closure',closureRoutes);
 module.exports=app;
