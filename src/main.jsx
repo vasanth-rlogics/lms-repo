9d7f4ad6d0334b9f44f039d60c63a8fbaf8494d6
vasanth-rlogics <vasanth.gopinath@rlogicssolutions.com>
2026-09-07 13:23:45 +0530

    Load unified black green theme after page styles

--- src/main.jsx
+++ src/main.jsx
@@ -2,5 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './global.css';
+import './theme.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(<React.StrictMode><App/></React.StrictMode>);
